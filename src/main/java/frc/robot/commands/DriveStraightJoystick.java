d71df3845106f408e9f01ad900818232dbf7fc1f
Jeremy N Chernin <203327@kcsd.org>
2019-01-22 16:19:07 -0500

    Added DriveStraightJoystick and GoToAngle commands

---
+++ src/main/java/frc/robot/commands/DriveStraightJoystick.java
@@ -0,0 +1,51 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.command.InstantCommand;
+import frc.robot.subsystems.TankDrive;
+
+/**
+ * Add your docs here.
+ */
+public class DriveStraightJoystick extends InstantCommand {
+  private double angle;
+
+  private Joystick joystick;
+  private TankDrive drive;
+  /**
+   * Add your docs here.
+   */
+  public DriveStraightJoystick(TankDrive drive, Joystick joystick) {
+    super();
+    this.requires(drive);
+    this.drive = drive;
+    this.joystick = joystick;
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+  }
+
+  // Called once when the command executes
+  @Override
+  protected void initialize() {
+    angle = drive.getHeading();
+  }
+  @Override
+  protected void execute() {
+    drive.setHeading(angle, -joystick.getY());
+  }
+  @Override
+  protected boolean isFinished(){
+    return false;
+  }
+  @Override
+  protected void end() {
+    drive.stop();
+  }
+}
---
+++ src/main/java/frc/robot/commands/GoToAngle.java
@@ -0,0 +1,54 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.InstantCommand;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.commands.tankdrive.DriveWithJoysticks;
+import frc.robot.subsystems.TankDrive;
+
+/**
+ * Add your docs here.
+ */
+public class GoToAngle extends InstantCommand {
+    private static final int MAX_ON_TARGET_COUNT = 10;
+    private int onTargetCounter;
+    protected TankDrive drive;
+    protected double angle;
+  /**
+   * Add your docs here.
+   */
+  public GoToAngle(TankDrive drive, double angle) {
+    super();
+    this.drive = drive;
+    this.angle = angle;
+    this.requires(drive);
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+  }
+
+  // Called once when the command executes
+  @Override
+  protected void initialize() {
+    drive.setHeading(angle, 0);
+    SmartDashboard.putNumber("Drive-Setpoint", angle);
+    onTargetCounter = 0;
+  }
+  @Override
+  protected boolean isFinished(){
+    if(drive.onTarget()) {
+      onTargetCounter++;
+    }
+    return onTargetCounter > MAX_ON_TARGET_COUNT || this.isTimedOut();
+  }
+  @Override
+  protected void end() {
+    drive.stop();
+  }
+
+}
