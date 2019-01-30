fb1089c7c9d172dc2c0e26e2a7ea5bb5b90210a0
Davis Piercy <davispiercy@gmail.com>
2019-01-30 15:42:26 -0500

    Added elevator commands

---
+++ src/main/java/frc/robot/commands/elevator/ElevateCargoToHigh.java
@@ -0,0 +1,27 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.elevator;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.subsystems.Elevator;
+
+/**
+ * Sets the height of the elevator to place cargo high
+ */
+public class ElevateCargoToHigh extends Elevate {
+	private static final double CARGO_PORT_HIGH = 0;
+
+	/**
+	 * Sets the height of the elevator to place cargo high
+	 * @param elevator the elevator
+	 */
+	public ElevateCargoToHigh(Elevator elevator) {
+		super(elevator, CARGO_PORT_HIGH);
+  }
+}
+
---
+++ src/main/java/frc/robot/commands/elevator/ElevateCargoToLow.java
@@ -0,0 +1,26 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.elevator;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.subsystems.Elevator;
+
+/**
+ * Sets the height of the elevator to place cargo low
+ */
+public class ElevateCargoToLow extends Elevate {
+	private static final double CARGO_PORT_LOW = 0;
+
+	/**
+	 * Sets the height of the elevator to place cargo low
+	 * @param elevator the elevator
+	 */
+	public ElevateCargoToLow(Elevator elevator) {
+		super(elevator, CARGO_PORT_LOW);
+	}
+}
